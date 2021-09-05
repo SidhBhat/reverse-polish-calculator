140aacb84818a9f0094f8d603b6f734787c58799
Siddharth Bhat <essdeehb@gmail.com>
2021-09-05 12:37:41 +0530

    Feature add...
    Added ability to process fractional and exponetial notation.
    thus real numbers can now be processed.

--- src/getop.c
+++ src/getop.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<ctype.h>
 #include"getch.h"
-
-#define NUMBER '0'
+#include"revrse_polish_calc.h"
 
 // getop: function to recognise operators and numbers ,writing number to to string
 int getop(char *const str)
@@ -12,14 +11,25 @@ int getop(char *const str)
 	while((str[0] = ch = getch()) == ' ' || ch == '\t')
 		;
 	str[1] = '\0';
-	if(!isdigit(ch) && ch != '.')
+	if(!isdigit(ch) && ch != '.' && ch != '-')
 		return ch;
+	if(ch == '-')
+		if(!isdigit(str[++i] = ch = getch()) && ch != '.') {
+			ungetch(ch);
+			str[i] = '\0';
+			return '-';
+		}
 	if(isdigit(ch))
 		while(isdigit(str[++i] = ch = getch()))
 			;
 	if(ch == '.')
 		while(isdigit(str[++i] = ch = getch()))
 			;
+	if(ch == 'e' || ch == 'E') {
+		if(isdigit(str[++i] = ch = getch()) || ch == '-')
+			while(isdigit(str[++i] = ch = getch()))
+			;
+	}
 	str[i] = '\0';
 	if(ch != EOF)
 		ungetch(ch);
